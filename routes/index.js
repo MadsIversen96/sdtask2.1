8706625c0e5c448642e1495ea8687f2c43e6e6e5
MadsIversen96 <madsmiversen@hotmail.no>
2023-10-30 11:26:24 +0100

    Fix

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = new express.Router();
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3()
-let content = require("../content.json");
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
- let my_file = await s3.getObject({
-    Bucket: "cyclic-fluffy-rose-pumps-eu-north-1",
-    Key: "content.json"
- }).promise()
- const result = JSON.parse(my_file.Body)?.content;
- if(!my_file.Body) {
-  res.json({
-    status: "fail"
-  })
- } else {
-  res.json({
-    status: "success",
-    content: result
-  })
- }
+  let my_file = await s3.getObject({
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json",
+  }).promise()
+  const result = JSON.parse(my_file.Body)?.content
+  if(result == null) {
+    res.json({
+      status: "fail"
+    });  
+  }
+  else {
+    res.json({
+      status: "success",
+      content: result,
+    });  
+  }
 });
 
-router.post('path', async (req, res) => {
-const {content} = req.body;
-const contentObj = {
-  content: content
-}
-await s3.putObject({
-  Body: JSON.stringify(contentObj, null, 2),
-  Bucket: "cyclic-fluffy-rose-pumps-eu-north-1",
-  Key: "content.json" 
-}).promise()
-if(content == null) {
-  res.json({
-    status: "fail"
-  })
-} else {
+router.post("/", async (req, res) => {
+  const {content} = req.body;
+  const contentObj = {
+    content: content
+  }
+  await s3.putObject({
+    Body: JSON.stringify(contentObj, null, 2),
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json",
+  }).promise()
   res.json({
     status: "success",
-    content: content
-  })
-}
+    content: content,
+  });
 });
 
 module.exports = router;
