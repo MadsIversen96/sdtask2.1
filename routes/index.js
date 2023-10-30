1802642460a94890929a4e5ff4bb5529967a5b71
MadsIversen96 <madsmiversen@hotmail.no>
2023-10-30 10:36:15 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+ let my_file = await s3.getObject({
+    Bucket: "",
+    Key: "content.json"
+ }).promise()
+ const result = JSON.parse(my_file.Body)?.content;
+ if(result == null) {
+  res.json({
+    status: "fail"
+  })
+ } else {
+  res.json({
+    status: "success",
+    content: result
+  })
+ }
+});
+
+router.post('path', async (req, res) => {
+const {content} = req.body;
+if(content == null) {
+  res.json({
+    status: "fail"
+  })
+} else {
+  res.json({
+    status: "success",
+    content: content
+  })
+}
+});
+
+module.exports = router;
